d2cf31780586004e1c259e7d838303fc45055e92
Libor Wagner <liborwagner@gmail.com>
2013-12-09 12:42:10 +0100

    Fix more typo

--- oris.py
+++ oris.py
@@ -19,31 +19,31 @@ class ORIS(object):
 
     def getCSOSClubList(self):
         url = self._make_request("getCSOSClubList")
-        return req._query(url)
+        return self._query(url)
 
     def getClub(self, id):
         url = self._make_request("getClub", {"id":id})
-        return req._query(url)
+        return self._query(url)
 
     def getEventList(self, all=None, name=None, sport=None, rg=None, datefrom=None, dateto=None):
         url = self._make_request("getEventList", {"all":all, "name":name, "sport":sport, "rg":rg, "detafrom":datefrom, "datato":dateto})
-        return req._query(url)
+        return self._query(url)
 
     def getEvent(self, id):
         url = self._make_request("getEvent", {"id":id})
-        return req._query(url)
+        return self._query(url)
 
     def getEventEntries(self, eventid, classid=None, classname=None, clubid=None, entrystop=None, entrystopout=None):
         url = self._make_request("getEventEntries", {"eventid":eventid, "classid":classid, "calssname":classname, "clubid":clubid, "entrystop":entrystop, "entrystopout":entrystopout})
-        return req._query(url)
+        return self._query(url)
 
     def getEventResults(self, eventid, classid=None, classname=None, clubid=None):
         url = self._make_request("getEventResults", {"eventid":eventid, "classid":classid, "classname":classname, "clubid":clubid})
-        return req._query(url)
+        return self._query(url)
 
     def getUser(self, rgnum):
         url = self._make_request("getUser", {"rgnum":rgnum})
-        return req._query(url)
+        return self._query(url)
 
     def _make_request(self, method, params={}):
         args = dict([("format",self.format), ("method",method)] + params.items())
