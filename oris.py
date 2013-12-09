667e319ca8f31c7bc3235ad9c19ab73c264c2e5f
Libor Wagner <liborwagner@gmail.com>
2013-12-09 12:36:20 +0100

    Initial implementation

--- oris.py
+++ oris.py
@@ -3,6 +3,12 @@
 # Author:      Libor Wagner <wagnelib@cmp.felk.cvut.cz>
 # Created on:  Dec 9, 2013
 
+
+import urllib2
+import urllib
+import json
+
+
 class ORIS(object):
     """Implementation of the ORIS API (http://oris.orientacnisporty.cz/API)
     """
@@ -12,23 +18,39 @@ class ORIS(object):
        self.uri = uri
 
     def getCSOSClubList(self):
-        pass
+        url = self._make_request("getCSOSClubList")
+        return req._query(url)
 
     def getClub(self, id):
-        pass
+        url = self._make_request("getClub", {"id":id})
+        return req._query(url)
 
     def getEventList(self, all=None, name=None, sport=None, rg=None, datefrom=None, dateto=None):
-        pass
+        url = self._make_request("getEventList", {"all":all, "name"=name, "sport"=sport, "rg"=rg, "detafrom":datefrom, "datato"=dateto})
+        return req._query(url)
 
     def getEvent(self, id):
-        pass
+        url = self._make_request("getEvent", {"id":id})
+        return req._query(url)
 
     def getEventEntries(self, eventid, classid=None, classname=None, clubid=None, entrystop=None, entrystopout=None):
-        pass
+        url = self._make_request("getEventEntries", {"eventid":eventid, "classid":classid, "calssname":classname, "clubid":clubid, "entrystop":entrystop, "entrystopout":entrystopout})
+        return req._query(url)
 
     def getEventResults(self, eventid, classid=None, classname=None, clubid=None):
-        pass
+        url = self._make_request("getEventResults", {"eventid":eventid, "classid":classid, "classname":classname, "clubid":clubid})
+        return req._query(url)
 
     def getUser(self, rgnum):
-        pass
+        url = self._make_request("getUser", {"rgnum":rgnum})
+        return req._query(url)
 
+    def _make_request(self, method, params={}):
+        args = dict([("format",self.format), ("method",method)] + params.items())
+        args = dict((k, v) for k, v in args.iteritems() if v)
+        url = urllib.urlencode(args)
+        print self.uri + "?" + url
+
+    def _query(self, url):
+        req = usrllib2.urlopen(url)
+        return req.read()
